71ef6f1364e67837a384f5fa8f48bfdad1520a04
Tolibjon <stolibjon123@gmail.com>
2023-07-13 20:53:32 +0500

    asa

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-i^u8#2^jgq-5z^j3xvm4c8268i((p3%4bu(m6nppu@a^j#=pe%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
